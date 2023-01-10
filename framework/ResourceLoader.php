d61b6df45ea49cb57dc3890fb73825551ae85406
Farhannivta <farhannivtasija@gmail.com>
2023-01-10 23:31:32 +0700

    menambah fungsi getParameter di ResourceLoader

--- framework/ResourceLoader.php
+++ framework/ResourceLoader.php
@@ -4,8 +4,23 @@ namespace Framework;
 
 class ResourceLoader{
 
-    public function __construct(Resource $resource){
-        $resource->log();
+    private $resource = null;
+
+    public function __construct(string $resource){
+        $parameters = $this->getAllParams($resource);
+        $inject_class = $this->getClassOfParam($parameters);
+        dd($inject_class);
+    }
+
+    private function getAllParams(string $resource): array{
+        $ref = new \ReflectionClass($resource);
+        return $ref->getMethod('getAll')->getParameters();
+    }
+
+    private function getClassOfParam(array $parameters): array{
+        return array_map(function($param){
+            
+        }, $parameters)
     }
 
 }
