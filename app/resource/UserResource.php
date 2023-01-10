e49aa63c60163525fe9216475aef22294654a41e
Farhannivta <farhannivtasija@gmail.com>
2023-01-10 23:30:29 +0700

    use requst dan implement resource di dalam UserResource

--- app/resource/UserResource.php
+++ app/resource/UserResource.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Resource;
+use Framework\Request\Request;
+use Framework\Mariana\Resource;
 
-class UserResource{
-    public function getAll(){
+class UserResource implements Resource{
+    public function getAll(Request $request){
         dd("hello world");
     }
 }
