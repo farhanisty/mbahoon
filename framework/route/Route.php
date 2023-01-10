fc471fe0badd53f2af27d3aa8b38937344d3ed3c
Farhannivta <farhannivtasija@gmail.com>
2023-01-10 23:34:32 +0700

    menambahkan add logic di route

--- framework/route/Route.php
+++ framework/route/Route.php
@@ -5,6 +5,7 @@ namespace Framework\Route;
 use Framework\Route\RouteDao;
 use Framework\Mariana\Resource;
 use Framework\ResourceLoader;
+use Framework\Request\Request;
 use App;
 
 class Route{
@@ -15,8 +16,20 @@ class Route{
     }
 
     public static function run(): void{
+        $request = Request::getInstance();
 
-        $resource_loader = new ResourceLoader();
-        dd(((new (self::$routes[0]->getResource())())->getAll()));
+        $is_registred_url = array_filter(self::$routes, function($route) use ( $request ) {
+            if($route->getUrl() == $request->getPath()){
+                return $route;
+            }
+        });
+
+        if(count($is_registred_url) !== 1){
+            throw new Exception("404");
+        }
+
+        $resource = $is_registred_url[0]->getResource();
+
+        $resource_loader = new ResourceLoader($resource);
     }
 }
