da5f5e25410b08c74480cf5e1a4c3f375c47f1ad
Farhannivta <farhannivtasija@gmail.com>
2023-01-10 23:33:44 +0700

    menyederhanakan resource interface

--- framework/mariana/Resource.php
+++ framework/mariana/Resource.php
@@ -3,9 +3,4 @@
 namespace Framework\Mariana;
 
 interface Resource{
-    public function get(): Response;
-    public function singleGet($id): Response;
-    public function post(): Response;
-    public function put($id): Response;
-    public function delete(): Response;
 }
